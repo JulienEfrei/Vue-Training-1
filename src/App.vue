63d9ce87357a5749ba2e0c98cbee64a65ad6225f
Julien Klein <44435573+iLdro@users.noreply.github.com>
2022-11-21 21:22:09 +0100

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+  <Card 
+  v-for="item in items"
+  :name = "item.name"
+  :price = "item.price"
+  :image = "item.image"
+  :isCold = "item.isCold"
+  :key="item"
+  />
+</div>
+</template>
+<script setup>
+import Card from "./components/card.vue";
+const image="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Roasted_coffee_beans.jpg/1200px-Roasted_coffee_beans.jpg";
+const items =[{
+  name: 'Vanilla Latte',
+  price: 4.99,
+  isCold: false,
+  image : image,
+}
+, {
+  name: 'Iced Coffee',
+  price: 3.99,
+  isCold: true,
+  image : image,
+},
+{
+  name: 'Cappuccino',
+  price: 5.99,
+  isCold: false,
+  image : image,
+},
+{
+  name: 'Iced Tea',
+  price: 2.99,
+  isCold: true,
+  image : image,
+}
+]
+
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="card">
+        <div class="card__image">
+            <img :src="props.image" alt="coffee" />
+        </div>
+        <div class="card__content">
+            <h2 class="card__title">{{ props.name }}</h2>
+            <p class="card__price">{{ props.price }}</p>
+            <p class="card__isCold">{{ props.isCold }}</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    name: String,
+    price: Number,
+    image: String,
+    isCold: Boolean,
+})
+</script>
+
+<style scoped>
+.card {
+    width: 300px;
+    height: 400px;
+    background-color: #fff;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px;
+    margin: 20px;
+}
+
+.card__image {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.card__image img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+
+.card__content {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.card__title {
+    font-size: 1.5rem;
+    font-weight: 500;
+    text-align: center;
+}
+
+.card__price {
+    font-size: 1.2rem;
+    font-weight: 500;
+    text-align: center;
+}
+
+.card__isCold {
+    font-size: 1.2rem;
+    font-weight: 500;
+    text-align: center;
+}
+
+</style>
